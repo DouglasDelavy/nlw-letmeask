184599fd7380879e626d5bd66fcdede3e73f7fe9
Oddo <45064803+OddoAkbar@users.noreply.github.com>
2021-06-22 19:57:30 -0300

    React Routes and firebase auth

--- src/index.tsx
+++ src/index.tsx
@@ -3,5 +3,6 @@ import ReactDOM from "react-dom";
 import App from "./App";
 
 import "./services/firebase";
+import "./styles/global.scss";
 
 ReactDOM.render(<App />, document.getElementById("root"));
