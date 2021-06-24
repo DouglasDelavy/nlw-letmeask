0b89c473e566e3cccc634ecb8cbfbacefec49829
Oddo <45064803+OddoAkbar@users.noreply.github.com>
2021-06-23 21:56:39 -0300

    Add Admin Page and services

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,18 +1,18 @@
+import React, { FormEvent, useState } from "react";
+
 import { useHistory } from "react-router-dom";
 
-import illustrationImg from "../assets/images/illustration.svg";
-import logoImg from "../assets/images/logo.svg";
-import googleIconImg from "../assets/images/google-icon.svg";
+import illustrationImg from "../../assets/images/illustration.svg";
+import logoImg from "../../assets/images/logo.svg";
+import googleIconImg from "../../assets/images/google-icon.svg";
 
-import { Button } from "../components/Button";
+import { Button } from "../../components/Button";
 
-import { useAuth } from "../hooks/useAuth";
+import { useAuth } from "../../hooks/useAuth";
 
-import { database } from "../services/firebase";
+import { database } from "../../services/firebase";
 
-import "../styles/auth.scss";
-import { FormEvent } from "react";
-import { useState } from "react";
+import "../../styles/auth.scss";
 
 export function Home() {
   const history = useHistory();
@@ -39,6 +39,11 @@ export function Home() {
 
     if (!roomRef.exists()) {
       alert("Room does not exist");
+      return;
+    }
+
+    if (roomRef.val().endedAt) {
+      alert("Room already closed");
       return;
     }
 
--- src/pages/NewRoom/index.tsx
+++ src/pages/NewRoom/index.tsx
@@ -1,13 +1,15 @@
-import React, { FormEvent } from "react";
+import React, { FormEvent, useState } from "react";
 import { Link, useHistory } from "react-router-dom";
-import illustrationImg from "../assets/images/illustration.svg";
-import logoImg from "../assets/images/logo.svg";
 
-import "../styles/auth.scss";
-import { Button } from "../components/Button";
-import { useState } from "react";
-import { database } from "../services/firebase";
-import { useAuth } from "../hooks/useAuth";
+import illustrationImg from "../../assets/images/illustration.svg";
+import logoImg from "../../assets/images/logo.svg";
+
+import { Button } from "../../components/Button";
+
+import { database } from "../../services/firebase";
+import { useAuth } from "../../hooks/useAuth";
+
+import "../../styles/auth.scss";
 
 export function NewRoom() {
   const { user } = useAuth();
